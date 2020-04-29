5589fef6e2750d58f5c892e4f3efc4490900816a
juaneteuy <63799995+juaneteuy@users.noreply.github.com>
2020-04-29 13:15:25 -0300

    ej10 sin hacer

---
+++ ejercicio_10_multiplicar_sin_reload/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+app.get("", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/multiplicar", (req, res) => {
+  const num1 = req.query.num1;
+  const num2 = req.query.num2;
+  res.send("El resultado es " + num1 * num2 + "");
+  res.end();
+});
+
+app.listen(3000);
