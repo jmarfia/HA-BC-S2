b78bf9f771d54ac9fd998e983628ad539ceb120a
juaneteuy <63799995+juaneteuy@users.noreply.github.com>
2020-05-07 19:13:29 -0300

    actualizo a como tenia el ej20

--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -1,34 +1,17 @@
 const http = require("http");
 const express = require("express");
+const dblocal = require("db.js")
 var bodyParser = require("body-parser");
-const mysql = require("mysql");
-
 const app = express();
+
 app.set("view engine", "ejs");
+
 app.use(express.json());
 
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "autores",
-});
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("¡Nos conectamos a la BD!");
-});
-
-
-let articulosdb = [];
-
 app.get("/", function (req, res) {
-  connection.query("SELECT * FROM autores", function (err, articulos) {
-    if (err) throw err;
-    articulosdb = JSON.stringify(articulos);
-    console.log(articulosdb);
-    connection.end();
-    res.render("home", { value: articulosdb });
+  traerUsuarios(function (usersdb) {
+    res.render("home", { users: usersdb });
+    //console.log(usersdb)
     res.end();
   });
 });
