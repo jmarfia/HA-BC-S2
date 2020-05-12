26908a6097df32c30c8d8076a8bd0a4954eaab15
juaneteuy <63799995+juaneteuy@users.noreply.github.com>
2020-05-12 12:42:35 -0300

    todo pasado a controller. Falta alguna ruta no mas

---
+++ ejercicio_blog/controller/postController.js
@@ -0,0 +1,76 @@
+const DBLocal = require("../db");
+
+
+module.exports = {
+
+  //trae todos los articulos, debe llamarse cuando queres ir a /
+  //index
+  getAllArticles(req, res) {
+    DBLocal.getData(function (articlesDB) {
+      res.render("home", { articulos: articlesDB });
+    }, DBLocal.queryAllArticles);
+  },
+
+  //trae un articulo, debes llamarla para ir a la vista de un articulo solo /articulo 
+  //show
+  getArticleById(req, res) {
+    const articuloID = req.query.articuloID;
+    DBLocal.getData(function (articlesDB) {
+      console.log(articlesDB);
+      res.render("articulo", { articulo: articlesDB });
+    }, DBLocal.queryGetArticle + articuloID);
+  },
+
+  //trae un articulo en la vista de edicion de articulo /modificararticulo
+  //edit
+  editArticleById(req, res) {
+    const articuloID = req.query.articuloID;
+    DBLocal.getData(function (articlesDB) {
+      //console.log(articlesDB);
+      res.render("modificararticulo", { articulo: articlesDB });
+    }, DBLocal.queryGetArticle + articuloID);
+  },
+
+  //te lleva a /adminpanel y muestra todos los articulos
+  adminPanel(req, res) {
+    DBLocal.getData(function (articlesDB) {
+      //console.log(articlesDB);
+      res.render("adminpanel", { articulos: articlesDB });
+    }, DBLocal.queryAllArticles);
+    //res.end(); no se por que no me anda si pongo el res.end
+  },
+
+  //tiene que ser un post, manda lo editado en la vista modificar articulo a la base, y redirige a /adminpanel donde se visualizara todo actualizado
+  //update
+  //setarticulo
+  updateArticleById(req, res) {
+    //post req.body
+    const articuloID = req.query.articuloID;
+    const articulomodificado = req.query.articulomodificado;
+    console.log(articuloID, articulomodificado);
+    DBLocal.getData(function (articlesDB) {
+      //console.log(articlesDB);
+      res.redirect("/adminpanel");
+    }, DBLocal.updateArticle1 +
+      "'" +
+      articulomodificado +
+      "'" +
+      DBLocal.updateArticle2 +
+      articuloID);
+  },
+
+  //ejecuta la consulta que borra un articulo y te redirige a /adminpanel
+  //destroy
+  deleteArticleById(req, res) {
+    const articuloID = req.query.articuloID;
+    DBLocal.getData(function (articlesDB) {
+      //console.log(articlesDB);
+      //res.render("adminpanel", { articulos: articlesDB });
+      res.redirect("/adminpanel");
+    }, DBLocal.queryDeleteArticle + articuloID);
+    //res.end(); no se por que no me anda si pongo el res.end
+  },
+  contacto (req, res) {
+    res.render("contacto");
+  }
+};
--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -1,74 +1,21 @@
 const http = require("http");
 const express = require("express");
 const DBLocal = require("./db");
+const postController = require("./controller/postController");
 const path = require("path");
 
 const app = express();
-
 app.set("view engine", "ejs");
-// app.use(express.static("/public"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  DBLocal.getData(function (articlesDB) {
-    res.render("home", { articulos: articlesDB });
-  }, DBLocal.queryAllArticles);
-});
 
-app.get("/articulo", function (req, res) {
-  const articuloID = req.query.articuloID;
-  DBLocal.getData(function (articlesDB) {
-    console.log(articlesDB);
-    res.render("articulo", { articulo: articlesDB });
-  }, DBLocal.queryGetArticle + articuloID);
-});
-
-app.get("/modificararticulo", function (req, res) {
-  const articuloID = req.query.articuloID;
-  DBLocal.getData(function (articlesDB) {
-    //console.log(articlesDB);
-    res.render("modificararticulo", { articulo: articlesDB });
-  }, DBLocal.queryGetArticle + articuloID);
-});
-
-app.get("/setarticulo", function (req, res) {
-  //post req.body
-  const articuloID = req.query.articuloID;
-  const articulomodificado = req.query.articulomodificado;
-  console.log(articuloID, articulomodificado);
-  DBLocal.getData(function (articlesDB) {
-    //console.log(articlesDB);
-    res.redirect("/adminpanel");
-  }, DBLocal.updateArticle1 + "'" + articulomodificado + "'" + DBLocal.updateArticle2 + articuloID);
-});
-
-app.get("/borrararticulo", function (req, res) {
-  const articuloID = req.query.articuloID;
-  DBLocal.getData(function (articlesDB) {
-    //console.log(articlesDB);
-    //res.render("adminpanel", { articulos: articlesDB });
-    res.redirect("/adminpanel");
-  }, DBLocal.queryDeleteArticle + articuloID);
-  //res.end(); no se por que no me anda si pongo el res.end
-});
-
-app.get("/adminpanel", function (req, res) {
-  DBLocal.getData(function (articlesDB) {
-    //console.log(articlesDB);
-    res.render("adminpanel", { articulos: articlesDB });
-  }, DBLocal.queryAllArticles);
-  //res.end(); no se por que no me anda si pongo el res.end
-});
-
-app.post("/asf", function (req, res) {
-  var query1 = req.body.fruta;
-  fruitList.push(query1);
-  res.render("index", { value: fruitList });
-});
-
-app.get("/contacto", function (req, res) {
-  res.render("contacto");
-});
+app.get("/", postController.getAllArticles);
+app.get("/articulo", postController.getArticleById);
+app.get("/modificararticulo", postController.editArticleById);
+app.get("/setarticulo", postController.updateArticleById);
+app.get("/borrararticulo", postController.deleteArticleById);
+app.get("/adminpanel", postController.adminPanel);
+app.get("/contacto", postController.contacto);
 
 app.listen(3000);
