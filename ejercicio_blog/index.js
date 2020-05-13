de4f68248658a16d2a9505ed749d7205da5e09ff
juaneteuy <63799995+juaneteuy@users.noreply.github.com>
2020-05-13 09:41:50 -0300

    a

--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -20,29 +20,4 @@ app.get("/contacto", postController.contacto);
 //router.get("/articulo/:id", (req, res) => { articleController.getOneArticle(req) }); //el de santiago
 //app/get("/articulos", funcionautomaticamente toma como parametro req y res)
 
-<<<<<<< HEAD
-
-////////////////////////////////////////////////////////////////////////////////////////
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('blogdb', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-app.listen(3000, console.log("Servidor en el puerto 3000"));
-=======
 app.listen(3000);
->>>>>>> parent of 0db6643... todo con controller
