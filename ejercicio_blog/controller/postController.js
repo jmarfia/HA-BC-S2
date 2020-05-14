2776de63b8c8cfcf009493ad283a26054d13852b
juaneteuy <63799995+juaneteuy@users.noreply.github.com>
2020-05-14 01:42:16 -0300

    gordo fisura mode: on

--- ejercicio_blog/controller/postController.js
+++ ejercicio_blog/controller/postController.js
@@ -1,5 +1,5 @@
 const db = require("../db");
-const {Author, Article} = require("../modelos")
+const { Author, Article } = require("../modelos");
 
 module.exports = {
   //trae todos los articulos, debe llamarse cuando queres ir a /
@@ -8,79 +8,71 @@ module.exports = {
     const articles = await Article.findAll({
       include: [Author],
     });
-    res.render("home", {articulos: articles});
+    res.render("home", { articulos: articles });
   },
 
   //trae un articulo, debes llamarla para ir a la vista de un articulo solo /articulo
   //show
-  async getArticleById(req, res){
+  async getArticleById(req, res) {
     const articuloID = req.query.articuloID;
-    console.log("asdasdfasdf",articuloID);
-    const articles = await Article.findByPk(articuloID,{
-      include: [Author]
+    const articles = await Article.findByPk(articuloID, {
+      include: [Author],
     });
-    console.log("ARTICLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", articles)
     res.render("articulo", { articulo: articles });
   },
-  
-  
-  // {
-  //   const articuloID = req.query.articuloID;
-  //   DBLocal.getData(function (articlesDB) {
-  //     console.log(articlesDB);
-  //     res.render("articulo", { articulo: articlesDB });
-  //   }, DBLocal.queryGetArticle + articuloID);
-  // },
 
   //trae un articulo en la vista de edicion de articulo /modificararticulo
   //edit
-  editArticleById(req, res) {
+  async editArticleById(req, res) {
     const articuloID = req.query.articuloID;
-    DBLocal.getData(function (articlesDB) {
-      //console.log(articlesDB);
-      res.render("modificararticulo", { articulo: articlesDB });
-    }, DBLocal.queryGetArticle + articuloID);
+    const articles = await Article.findByPk(articuloID, {
+      include: [Author],
+    });
+    res.render("modificararticulo", { articulo: articles });
   },
 
   //te lleva a /adminpanel y muestra todos los articulos
-  adminPanel(req, res) {
-    DBLocal.getData(function (articlesDB) {
-      //console.log(articlesDB);
-      res.render("adminpanel", { articulos: articlesDB });
-    }, DBLocal.queryAllArticles);
-    //res.end(); no se por que no me anda si pongo el res.end
+  async adminPanel(req, res) {
+    const articles = await Article.findAll({
+      include: [Author],
+    });
+    res.render("adminpanel", { articulos: articles });
   },
 
   //tiene que ser un post, manda lo editado en la vista modificar articulo a la base, y redirige a /adminpanel donde se visualizara todo actualizado
   //update
   //setarticulo
-  updateArticleById(req, res) {
+  async updateArticleById(req, res) {
     //post req.body
     const articuloID = req.query.articuloID;
     const articulomodificado = req.query.articulomodificado;
-    console.log(articuloID, articulomodificado);
-    DBLocal.getData(function (articlesDB) {
-      //console.log(articlesDB);
-      res.redirect("/adminpanel");
-    }, DBLocal.updateArticle1 +
-      "'" +
-      articulomodificado +
-      "'" +
-      DBLocal.updateArticle2 +
-      articuloID);
+    //console.log(articuloID, articulomodificado);
+
+    await Article.findByPk(articuloID).then((articleDB) => {
+      articleDB.update({
+        contenido: articulomodificado,
+      });
+    });
+    const articles = await Article.findByPk(articuloID, {
+      include: [Author],
+    });
+
+    res.render("articulo", { articulo: articles });
   },
 
   //ejecuta la consulta que borra un articulo y te redirige a /adminpanel
   //destroy
-  deleteArticleById(req, res) {
+ async deleteArticleById(req, res) {
     const articuloID = req.query.articuloID;
-    DBLocal.getData(function (articlesDB) {
-      //console.log(articlesDB);
-      //res.render("adminpanel", { articulos: articlesDB });
+   await Article.destroy({
+      where: {
+        id: articuloID,
+      },
+    }).then(() => {
       res.redirect("/adminpanel");
-    }, DBLocal.queryDeleteArticle + articuloID);
-    //res.end(); no se por que no me anda si pongo el res.end
+    });
   },
+
   contacto(req, res) {
     res.render("contacto");
   },
--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -10,17 +10,13 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", postController.getAllArticles);
-app.get("/articulo", postController.getArticleById);
-app.get("/modificararticulo", postController.editArticleById);
-app.get("/setarticulo", postController.updateArticleById);
-app.get("/borrararticulo", postController.deleteArticleById);
-app.get("/adminpanel", postController.adminPanel);
-app.get("/contacto", postController.contacto);
+app.get("/", postController.getAllArticles); //ok
+app.get("/articulo", postController.getArticleById); //ok
+app.get("/modificararticulo", postController.editArticleById);//ok
+app.get("/setarticulo", postController.updateArticleById);//ok
+app.get("/borrararticulo", postController.deleteArticleById);// ok
+app.get("/adminpanel", postController.adminPanel); //ok
+app.get("/contacto", postController.contacto); //ok
 //app.get("/pruebasqlz", postController.sqlz);
-
-//router.get("/articulo/:id", (req, res) => { articleController.getOneArticle(req) }); //el de santiago
-//app/get("/articulos", funcionautomaticamente toma como parametro req y res)
-//articleModel.getArticles2((data) => console.log(data))
 
 app.listen(3000);
