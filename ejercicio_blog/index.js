4f2d4e69039bc78911f1f7861f3526de4eb6ff64
fabrilacruz <63807751+fabrilacruz@users.noreply.github.com>
2020-05-15 14:18:42 -0300

    Añadido el form, gitignore y realizadas las instalaciones

--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -5,6 +5,11 @@ const postController = require("./controller/postController");
 const path = require("path");
 const articleModel = require("./modelos/article")
 
+//Requires Login/Register
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
 const app = express();
 app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -18,5 +23,13 @@ app.get("/borrararticulo", postController.deleteArticleById);// ok
 app.get("/adminpanel", postController.adminPanel); //ok
 app.get("/contacto", postController.contacto); //ok
 //app.get("/pruebasqlz", postController.sqlz);
+
+//Login/Register EndPoints
+app.get("/registro", (req, res) => {});
+app.post("/registro", (req, res) => {});
+app.get("/login", (req, res) => {});
+app.post("/login", (req, res) => {});
+app.get("/logout", (req, res) => {});
+
 
 app.listen(3000);
