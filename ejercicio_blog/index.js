dbdf4e1ad98a337a6fb8d898e4d9c0f70132eaeb
rndelpuerto <63263893+rndelpuerto@users.noreply.github.com>
2020-05-18 14:55:06 -0300

    update _login.js

--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -63,11 +63,11 @@ passport.deserializeUser(function (email, done) {
     done(err, user);
   });
 });
+
 //bcrypt
 const saltRounds = 10;
 const myPlaintextPassword = "s0//P4$$w0rD";
 const someOtherPlaintextPassword = "not_bacon";
-
 
 // Middleware de acceso.
 const access = () => {
