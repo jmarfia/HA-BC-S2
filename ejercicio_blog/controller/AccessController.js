c0f8ccbfd1496dcc837a85889ea21c1ad30a2e00
rndelpuerto <63263893+rndelpuerto@users.noreply.github.com>
2020-05-18 14:07:18 -0300

    update AccessController, update index.js

--- ejercicio_blog/controller/AccessController.js
+++ ejercicio_blog/controller/AccessController.js
@@ -1,73 +1,52 @@
 const db = require("../db");
 const { Author } = require("../modelos");
-
+const passport = require("passport");
 
 class AccessController {
+  constructor() {}
 
-    constructor() {}
+  static showLogin(req, res) {
+    const pageData = {
+      title: "Ingresar",
+      partialView: {
+        path: __dirname + '/../views/access/_login.ejs',
+        data: {},
+      },
+    };
 
+    res.render("access/index", { pageData });
+  }
 
-    static showLogin(req, res) {
+  static showRegister(req, res) {
+    const pageData = {
+      title: "Registro",
+      partialView: {
+        path: __dirname + '/../views/access/_register.ejs',
+        data: {},
+      },
+    };
 
-        const pageData = {
-            title: 'Ingresar',
-            partialView: {
-                path: 'access/_login',
-                data: {}
-            }
-        }
+    res.render("access/index", { pageData });
+  }
 
-        res.render('access/index', { pageData });
+  static login(req, res) {
+    passport.authenticate("local", {
+      successRedirect: "/admin",
+      failureRedirect: "/admin/ingresar",
+    });
+  }
+
+  static register(req, res) {
+    if (true) {
+      res.redirect("/admin");
+    } else {
+      res.redirect("/admin/ingreso");
     }
+  }
 
-
-    static showRegister(req, res) {
-
-        const pageData = {
-            title: 'Registro',
-            partialView: {
-                path: 'access/_register',
-                data: {}
-            }
-        }
-
-        res.render('access/index', { pageData });
-    }
-
-
-    static login(req, res) {
-        passport.authenticate("local", {    successRedirect: "/admin",    failureRedirect: "/admin/ingresar",  }) 
-
-     //   if(true)
-       // {
-       //     res.redirect('/admin');
-       // }
-       // else
-       // {
-         //   res.redirect('/admin/ingreso');
-       // }
-        
-    }
-
-
-    static register(req, res) {
-
-        if(true)
-        {
-            res.redirect('/admin');
-        }
-        else
-        {
-            res.redirect('/admin/ingreso');
-        }
-    }
-
-
-    static logout(req, res) {
-
-        res.redirect('/');
-    }
+  static logout(req, res) {
+    res.redirect("/");
+  }
 }
-
 
 module.exports = AccessController;
--- ejercicio_blog/index.js
+++ ejercicio_blog/index.js
@@ -4,69 +4,75 @@ const db = require("./db");
 const postController = require("./controller/postController");
 const AccessCtrl = require("./controller/AccessController");
 const path = require("path");
-const articleModel = require("./modelos/article")
+const articleModel = require("./modelos/article");
 const Sequelize = require("sequelize");
 
 //Requires Login/Register
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
-
-//Configs a mover de lugar
-//Session
-app.use(
-    session({
-    secret: "AlgúnTextoSuperSecreto",
-    resave: false,
-    saveUninitialized: false,
-    })
-   );
-   
-//Passport
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-  },
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
-//bcrypt
-const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-
+const bcrypt = require("bcryptjs");
 
 const app = express();
+
 app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
+//Configs a mover de lugar
+//Session
+app.use(
+  session({
+    secret: "AlgúnTextoSuperSecreto",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    function (username, password, done) {
+      User.findOne({ where: { email: username } }, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function (email, done) {
+  User.findOne({ where: { email: email } }, function (err, user) {
+    done(err, user);
+  });
+});
+//bcrypt
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+
 app.get("/", postController.getAllArticles); //ok
 app.get("/articulo", postController.getArticleById); //ok
-app.get("/modificararticulo", postController.editArticleById);//ok
-app.get("/setarticulo", postController.updateArticleById);//ok
-app.get("/borrararticulo", postController.deleteArticleById);// ok
+app.get("/modificararticulo", postController.editArticleById); //ok
+app.get("/setarticulo", postController.updateArticleById); //ok
+app.get("/borrararticulo", postController.deleteArticleById); // ok
 app.get("/adminpanel", postController.adminPanel); //ok
 app.get("/contacto", postController.contacto); //ok
 //app.get("/pruebasqlz", postController.sqlz);
@@ -74,9 +80,10 @@ app.get("/contacto", postController.contacto); //ok
 //Login/Register EndPoints
 app.get("/admin/registro", (req, res) => AccessCtrl.showRegister(req, res));
 app.post("/admin/registro", (req, res) => AccessCtrl.register(req, res));
-app.get("/admin/ingreso", (req, res) => AccessCtrl.showLogin(req, res));
-app.post("/admin/ingreso", (req, res) => { AccessController.login(passport, req, res,) });
-app.get("/admin/cerrar-sesion", (req, res) => AccessCtrl.logout(req, res));
-
+app.get("/admin/ingresar", (req, res) => AccessCtrl.showLogin(req, res));
+app.post("/admin/ingresar", (req, res) => AccessCtrl.login(req, res));
+app.get("/admin/cerrar-sesion", (req, res) =>
+  AccessCtrl.logout(passport, req, res)
+);
 
 app.listen(3000);
